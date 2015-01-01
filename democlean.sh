d2e1c7eed38b8b923508d4625c08bf708d7baac9
Fohlen <fohlen@einhufer.info>
2015-01-01 17:27:19 +0100

    Only remove files (do not touch the demo folders)

--- democlean.sh
+++ democlean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This script will delete all files older than 1 week
 # Run as a cronjob with: @daily /home/usr/democlean.sh
-find demos -mtime +7 -exec rm {} \;
+find demos -type f -mtime +7 -exec rm {} \;
 
